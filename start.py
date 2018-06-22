9d704b8bab936c787019d188f6088d480060e63f
roman412 <romanio412@gmail.com>
2018-06-22 03:17:11 +0300

    .py script for parsing android .thumbcache database into jpegs

---
+++ start.py
@@ -0,0 +1,21 @@
+f = open('d:\\pyth\\thumbdata3.dat', 'rb')
+tdata = f.read()
+f.close()
+
+ss = '\xff\xd8'
+se = '\xff\xd9'
+
+count = 0
+
+while True:
+    x1 = tdata.find(b'\xff\xd8')
+    if x1 < 0:
+        break
+    x2 = tdata.find(b'\xff\xd9')
+    jpg = tdata[x1:x2+1]
+    count += 1
+    fname = 'extracted%d03.jpg' % count
+    fw = open(fname, 'wb')
+    fw.write(jpg)
+    fw.close()
+    tdata = tdata[x2+2:]
